22e30dd3e69d571afb9e7d7f41d26a8a3a40e244
Esref Durna <edurna@gmail.com>
2016-12-24 03:00:11 -0800

    first commit

---
+++ docs/zh/usage.md
@@ -0,0 +1,55 @@
+## Usage
+
+### Simplest
+
+简单的例子：
+
+```js
+class SimplestSampleComponent extends React.Component {
+
+  ...
+
+  render() {
+    return (
+      <div>
+        <div className="btn-group">
+          {this.renderButtons()}
+        </div>
+        <AutoResponsive ref="container" {...this.getAutoResponsiveProps()}>
+          {this.renderItems()}
+        </AutoResponsive>
+      </div>
+    );
+  }
+}
+```
+
+### waterfall
+
+实现一个流式布局变得非常容易
+
+```js
+class WaterfallSampleComponent extends React.Component {
+
+  ...
+
+  render() {
+    return (
+      <AutoResponsive ref="container" {...this.getAutoResponsiveProps()}>
+        {
+          arrayList.map(function(i) {
+            return <div onClick={this.clickItemHandle} className="item" style={this.state.styleList[i]}>{i}</div>;
+          }, this)
+        }
+      </AutoResponsive>
+    );
+  }
+}
+
+```
+
+## Examples
+
+[示例1](./examples)
+
+[示例2 - Native](https://github.com/xudafeng/autoresponsive_react_native_sample)
---
+++ homepage/markdown.js
@@ -0,0 +1,78 @@
+/* ================================================================
+ * autoresponsive-react by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Mon Jun 02 2014 20:15:51 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright 2014 xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
+let React = require('react');
+let Markdown = require('marked');
+let Highlight = require('highlight.js');
+
+const events = ['clickHandler'];
+
+Markdown.setOptions({
+  highlight: function(code) {
+    return Highlight.highlightAuto(code).value;
+  }
+});
+
+class MarkdownComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      classNames: ['markdown']
+    };
+    this.bindEventMapContext();
+  }
+
+  bindEventMapContext() {
+    events.forEach(i => {
+      this[i] = this[i].bind(this);
+    });
+  }
+
+  componentWillMount() {
+    setTimeout(() => {
+      this.setState({
+        classNames: ['markdown', 'fadeIn']
+      });
+    }, 0);
+  }
+
+  clickHandler(e) {
+    let target = e.target;
+    let nodeName = target.nodeName;
+
+    if (nodeName !== 'H2' && nodeName !== 'H3') {
+      return;
+    }
+
+    if (target.id) {
+      location.href = location.href.split('#')[0] + '#' + target.id;
+    }
+  }
+
+  getClassNames() {
+    return this.state.classNames.join(' ');
+  }
+
+  render() {
+    return (
+      <div onClick={this.clickHandler} className={this.getClassNames()} dangerouslySetInnerHTML = {
+        {
+          __html: Markdown(this.props.children)
+        }
+      }></div>
+    );
+  }
+}
+
+module.exports = MarkdownComponent;
